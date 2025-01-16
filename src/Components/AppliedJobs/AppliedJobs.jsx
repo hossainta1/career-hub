cc202ca8a6cfd774d906100a40577a74eb8c4098
Tanvir Hossain <tanvir.hossain303030@gmail.com>
2025-01-16 18:44:51 +0600

    Job filter

--- src/Components/AppliedJobs/AppliedJobs.jsx
+++ src/Components/AppliedJobs/AppliedJobs.jsx
@@ -3,8 +3,27 @@ import { useLoaderData } from "react-router-dom";
 import { getStoredJobApplication } from "../../utility/LocalStorage";
 
 const AppliedJobs = () => {
+
   const jobs = useLoaderData();
+
   const [appliedJobs, setAppliedJobs] = useState([]);
+  const [displayJobs, setDisplayJobs] =useState([]);
+
+  const handleJobFilter = filter => {
+        if(filter === 'all'){
+        setDisplayJobs(appliedJobs);
+        }
+        else if(filter === 'remote'){
+            const remoteJobs = appliedJobs.filter( job => job.remote_or_onsite === 'Remote');
+            setDisplayJobs(remoteJobs);
+        }
+
+        else if (filter === 'Onsite'){
+            const onSiteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Onsite');
+            setDisplayJobs(onSiteJobs);
+        }
+  }
+
   useEffect(() => {
     const storedJobsIds = getStoredJobApplication();
     if (jobs.length > 0) {
@@ -17,28 +36,29 @@ const AppliedJobs = () => {
         }
       }
       setAppliedJobs(jobsApplied);
+      setDisplayJobs(jobsApplied);
     }
-  }, []);
+  }, [jobs]);
   return (
     <div>
       <h2 className="text-2xl">This is Applied Jobs: {appliedJobs.length}</h2>
       <details className="dropdown">
         <summary className="btn m-1">open or close</summary>
         <ul className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
-          <li>
+          <li onClick={() => handleJobFilter('all')}>
             <a>All</a>
           </li>
-          <li>
+          <li onClick={() => handleJobFilter('remote')}>
             <a>Remote</a>
           </li>
-          <li>
+          <li onClick={() => handleJobFilter('Onsite')}>
             <a>On site</a>
           </li>
         </ul>
       </details>
 
       {
-      appliedJobs.map((job) => (
+      displayJobs.map((job) => (
         <li key={job.id}>
           <span>
             {job.job_title} {job.company_name} : {job.remote_or_onsite}
